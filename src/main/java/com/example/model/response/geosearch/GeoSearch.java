03277e7890d0bce3f312a93abc2c13bdf6566926
Christopher Anthony Balcita <christopher.balcita@iress.com>
2024-03-11 21:34:33 +0800

    cleaning

--- src/main/java/com/example/model/response/geosearch/GeoSearch.java
+++ src/main/java/com/example/model/response/geosearch/GeoSearch.java
@@ -2,8 +2,6 @@ package com.example.model.response.geosearch;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GeoSearch(String title, double lat, double lon, String country) {
 }
