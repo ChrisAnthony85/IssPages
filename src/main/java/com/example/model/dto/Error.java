fe0e23264c619dacc9e677e11acd63ad547a7a4b
Christopher Anthony Balcita <christopher.balcita@iress.com>
2024-03-12 19:38:56 +0800

    reflection try

--- src/main/java/com/example/model/dto/Error.java
+++ src/main/java/com/example/model/dto/Error.java
@@ -1,10 +1,12 @@
 package com.example.model.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@RegisterForReflection
 public class Error extends ApiResponse {
     private String type;
     private int status;
--- src/main/java/com/example/model/dto/ResultsDTO.java
+++ src/main/java/com/example/model/dto/ResultsDTO.java
@@ -1,10 +1,12 @@
 package com.example.model.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@RegisterForReflection
 public class ResultsDTO extends ApiResponse {
     List<PlaceDTO> results;
 }
